13a1681bd725e5c1bcd2f797c4ba944c2970344b
Hungdeptrai28 <hungvivu28@gmail.com>
2024-07-10 20:52:48 +0700

    đã đổi tên trong footer

--- layout/footer.php
+++ layout/footer.php
@@ -5,7 +5,7 @@
         </div>
         <div class="float-end">
             <p>Developed <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-                by <a href="https://www.facebook.com/huyngudeochiuduoc">Phạm Gia Huy - PGH DEV</a></p>
+                by <a href="https://www.facebook.com/huyngudeochiuduoc">Nhóm 2</a></p>
         </div>
     </div>
 </footer>
